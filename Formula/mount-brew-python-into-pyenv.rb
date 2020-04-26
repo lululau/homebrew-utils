5718fd994066b75a830ea504d2218ee97834cc2c
Liu Xiang <liuxiang@ktjr.com>
2020-04-26 17:38:28 +0800

    Fix typo

--- Formula/mount-brew-python-into-pyenv.rb
+++ Formula/mount-brew-python-into-pyenv.rb
@@ -1,5 +1,5 @@
 class MountBrewPythonIntoPyenv < Formula
-  desc "Mouse Homebrew installed python into pyenv"
+  desc "Mount Homebrew installed python into pyenv"
   homepage "https://liuxiang.coding.net/p/utils/d/utils/git/tree/master"
   url "https://liuxiang.coding.net/p/utils/d/utils/git/raw/master/mount-brew-python-into-pyenv/mount-brew-python-into-pyenv", using: :nounzip
   sha256 "8c8f38b99cbece231e0771f4692eed8ced513753043aa0970e1b3e2f19efef13"
