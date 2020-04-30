33c8d0f63fc26cacb34160e64aa38b1c50e1c140
Liu Xiang <liuxiang@ktjr.com>
2020-04-30 16:22:32 +0800

    Update checksum

--- Formula/maven-artifact-refresh.rb
+++ Formula/maven-artifact-refresh.rb
@@ -2,7 +2,7 @@ class MavenArtifactRefresh < Formula
   desc "A small script for refresh artifact in local .m2 Maven repository"
   homepage "https://liuxiang.coding.net/p/utils/d/utils/git/tree/master"
   url "https://liuxiang.coding.net/p/utils/d/utils/git/raw/master/maven-artifact-refresh/maven-artifact-refresh", using: :nounzip
-  sha256 "8eb135b6b37e90180d81532e668647fad4b90d5ec1be69a219492f0ad94094c1"
+  sha256 "98e9136812a6c358ed2c48399571972f5ff020016fa4e90629b5f7c9c30b5ee1"
   version "1.0.0"
 
   depends_on "fzf" => :recommended
