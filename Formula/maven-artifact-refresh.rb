2448a9074b1261ab0f292e769ebd9be5589e3c53
Liu Xiang <liuxiang@ktjr.com>
2020-05-26 11:02:55 +0800

    Bump maven-artifact-refresh version

--- Formula/maven-artifact-refresh.rb
+++ Formula/maven-artifact-refresh.rb
@@ -1,9 +1,9 @@
 class MavenArtifactRefresh < Formula
   desc "A small script for refresh artifact in local .m2 Maven repository"
   homepage "https://liuxiang.coding.net/p/utils/d/utils/git/tree/master"
-  url "https://liuxiang.coding.net/p/utils/d/utils/git/raw/master/maven-artifact-refresh/maven-artifact-refresh-1.0.0.zip"
-  sha256 "5c47e061b7304cae65b311712cb993177ed426c28e8d523453c0e69d73e7144e"
-  version "1.0.0"
+  url "https://liuxiang.coding.net/p/utils/d/utils/git/raw/master/maven-artifact-refresh/maven-artifact-refresh-1.0.1.zip"
+  sha256 "60919e4a77f347e9fd27439ab58a481b98a1c4a1314416fd331e75ce042fa400"
+  version "1.0.1"
 
   depends_on "fzf" => :recommended
 
