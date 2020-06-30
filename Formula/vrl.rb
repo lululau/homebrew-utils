7e854611cb01221a2839b6441962de01a18125eb
Liu Xiang <liuxiang@ktjr.com>
2020-06-30 12:04:06 +0800

    Update

--- Formula/vrl.rb
+++ Formula/vrl.rb
@@ -2,7 +2,7 @@ class Vrl < Formula
   desc "Simple script for view logs on remote server via lnav"
   homepage "https://liuxiang.coding.net/p/utils/d/utils/git/tree/master"
   url "https://liuxiang.coding.net/p/utils/d/utils/git/raw/master/vrl/vrl", using: :nounzip
-  sha256 "10cebe7f8b04557012e5156f93fdb42445cfac40982244da3939cb03d413b0e6"
+  sha256 "a918b318594fa433964152f68a54297efaeb979f1b89f6a8f1924b46c133c244"
   version "1.0.0"
 
   depends_on "fzf" => :recommended
