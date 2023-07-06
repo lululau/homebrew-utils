bad20d0d578aa5e5b3262acef774202cb18e883b
Liu Xiang <liuxiang@ktjr.com>
2023-07-06 17:54:04 +0800

    Update ccal to 1.0.1

--- Formula/ccal.rb
+++ Formula/ccal.rb
@@ -1,9 +1,9 @@
 class Ccal < Formula
   desc "CLI China Lunar Calender"
   homepage "https://github.com/lululau/utils/tree/master"
-  url "https://github.com/lululau/utils/raw/master/ccal/ccal-1.0.0.zip"
-  sha256 "e97f45e769283bc35df4b46047a652b9a21b44d54043168cb096d4dcb8c74a4a"
-  version "1.0.0"
+  url "https://github.com/lululau/utils/raw/master/ccal/ccal-1.0.1.zip"
+  sha256 "e3c0e50d859d0d777494eae875307e3c2565ef84753db54fc402f129f33328e7"
+  version "1.0.1"
 
   def install
     bin.install 'ccal'
