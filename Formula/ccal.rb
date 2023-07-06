2a1d7976f4021d8de20512d0f259a4990c3a7867
Liu Xiang <liuxiang@ktjr.com>
2023-07-06 17:32:21 +0800

    Update digest for ccal-1.0.0.zip

--- Formula/ccal.rb
+++ Formula/ccal.rb
@@ -2,7 +2,7 @@ class Ccal < Formula
   desc "CLI China Lunar Calender"
   homepage "https://liuxiang.coding.net/p/utils/d/utils/git/tree/master"
   url "https://liuxiang.coding.net/p/utils/d/utils/git/raw/master/ccal/ccal-1.0.0.zip"
-  sha256 "e97f45e769283bc35df4b46047a652b9a21b44d54043168cb096d4dcb8c74a4a"
+  sha256 "cc2aee1dda6fbab84e2c50869d6d5b7c7232b8ca13128ac02c2f32a264d75713"
   version "1.0.0"
 
   def install
