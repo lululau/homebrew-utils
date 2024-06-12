395d86b56d7405cc2a013149a00fb8093e109306
Liu Xiang <liuxiang921@gmail.com>
2024-06-12 11:30:02 +0800

    Add fdk

---
+++ Formula/fdk.rb
@@ -0,0 +1,14 @@
+class Fdk < Formula
+  desc "Docker CLI client using fzf"
+  homepage "https://github.com/lululau/utils/tree/master"
+  url "https://github.com/lululau/fzf-docker/archive/refs/tags/1.0.0.zip"
+  sha256 "c952a6cc1217274d772ea290ff012072a2980830b544dd65e04860bb1ade2e0a"
+  version "1.0.0"
+
+  def install
+    bin.install 'fdk'
+    bin.install 'fdkx'
+    bin.install 'fdki'
+    bin.install 'fdkc'
+  end
+end
