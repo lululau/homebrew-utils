2f437c2c068937daafb57e0edc6ae5f0b657eadb
Liu Xiang <liuxiang921@gmail.com>
2024-06-28 13:28:40 +0800

    Upgrade fdk to 1.0.1

--- Formula/fdk.rb
+++ Formula/fdk.rb
@@ -1,9 +1,9 @@
 class Fdk < Formula
   desc "Docker CLI client using fzf"
   homepage "https://github.com/lululau/utils/tree/master"
-  url "https://github.com/lululau/fzf-docker/archive/refs/tags/1.0.0.zip"
-  sha256 "c952a6cc1217274d772ea290ff012072a2980830b544dd65e04860bb1ade2e0a"
-  version "1.0.0"
+  url "https://github.com/lululau/fzf-docker/archive/refs/tags/1.0.1.zip"
+  sha256 "86a1154eb9d13f84c656ae602a4b1552bc5cc86db1997b2b073063980c8244cc"
+  version "1.0.1"
 
   depends_on "fzf"
 
