01b2d37a23d47e80ba22460db9d97f1bd7c7386f
Liu Xiang <liuxiang921@gmail.com>
2026-09-03 10:36:06 +0800

    git-multies 升级至 1.1.0（更新 git-multi-status），移除单独的 git-multi-status formula

--- Formula/git-multies.rb
+++ Formula/git-multies.rb
@@ -1,9 +1,9 @@
 class GitMulties < Formula
   desc "Git sub-commands for run git-pull, git-status, git-branch on sub-directories"
   homepage "https://github.com/lululau/utils/tree/master"
-  url "https://github.com/lululau/utils/raw/master/git-multies/git-multies-1.0.0.zip"
-  sha256 "4d2635b1875cccc89ebb9941697785768ae8010b0562d5190b239a143f76f180"
-  version "1.0.0"
+  url "https://github.com/lululau/utils/raw/master/git-multies/git-multies-1.1.0.zip"
+  sha256 "fe6c43e6a0b0ca8d1cfb4be1309ce597016acd46d8a36b2b2dcc88d6c4680dba"
+  version "1.1.0"
 
   def install
     bin.install 'git-multi-pull'
